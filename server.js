dac95f6730edb92eed40a759cc7a18aa51bc4eb7
J. Nick Baughan <JNickBaughan@Gmail.com>
2023-06-17 13:01:12 -0400

    add basic register and login routes

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const server = express();
+server.use(express.json());
+
+const saltRounds = 10;
+
+const getDataStore = () => {
+    const store = {};
+
+    const authenticateUser = async ({name, password }) => {
+        // TODO add safety checks
+        if(store[name]){ 
+            const match = await bcrypt.compare(password, store[name].password);
+            // TODO send JWT in future
+            if(match){
+                return {
+                    error: false
+                }
+            }
+            return {
+                error: true
+            }
+        }
+        return {
+            error: true
+        }
+    }
+
+    const addUser = async ({name, password}) => {
+        // TODO add safety checks
+        if(store[name]){
+            return {
+                error: true
+            }
+        }
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(password, salt);
+        if(hash){
+            store[name] = {
+                name,
+                password: hash
+            }
+    
+            return {
+                error: false
+            }
+        }
+
+        return {
+            error: true
+        }
+        
+    }
+
+    return {
+        authenticateUser,
+        addUser
+    }
+
+}
+
+const dataStore = getDataStore();
+
+
+server.post("/register", async (req, res) => {
+    const { name, password } = req.body;
+    const result = await dataStore.addUser({ name, password });
+    if(!result.error){
+        res.status(200).send(`${name} has been registered`);
+    }else{
+        res.status(400).send(`There was an issue registering user ${name}`);
+    }
+});
+
+server.post("/login", async (req, res) => {
+    const { name, password } = req.body;
+    const result = await dataStore.authenticateUser({ name, password });
+    if(!result.error){
+        res.status(200).send(`${name} is authenticated`);
+    }else{
+        res.status(400).send(`There was an issue authenticating user ${name}`);
+    }
+});
+
+server.listen(3000, () => {
+    console.log(`Server running on port 3000`);
+});
